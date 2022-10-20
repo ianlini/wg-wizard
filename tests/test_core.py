8c10b13fbf94e5ee7181fb5bfe3752632061c678
Ian Lin <4056670+ianlini@users.noreply.github.com>
2022-10-20 22:57:06 +0800

    first test

---
+++ tests/test_core.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+
+from wg_wizard.core import WgWizard
+
+parent_dir = Path(__file__).parent
+
+
+@pytest.mark.parametrize(
+    "config_dir, interface", [(parent_dir / "data" / "default_with_one_client", "wg0")]
+)
+def test_read_wg_wizard(config_dir, interface):
+    wg_wizard = WgWizard.from_dir(config_dir, interface)
+    wg_wizard.check_secret()
