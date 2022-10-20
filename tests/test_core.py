89d4420d91cd8c2bb1e24829511b62a94d1486ff
Ian Lin <4056670+ianlini@users.noreply.github.com>
2022-10-20 23:27:33 +0800

    fix file mode

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pytest
 
 from wg_wizard.core import WgWizard
+from wg_wizard.paths import get_secret_path
 
 parent_dir = Path(__file__).parent
 
@@ -11,5 +12,6 @@ parent_dir = Path(__file__).parent
     "config_dir, interface", [(parent_dir / "data" / "default_with_one_client", "wg0")]
 )
 def test_wg_wizard_from_dir(config_dir, interface):
+    get_secret_path(config_dir, interface).chmod(mode=0o600)
     wg_wizard = WgWizard.from_dir(config_dir, interface)
     wg_wizard.check_secret()
